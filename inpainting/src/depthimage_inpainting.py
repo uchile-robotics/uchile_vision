1736edde040e4c2041b486b3c679e3a835d27647
EduJorquera <eduardo.jorquera@ug.uchile.cl>
2023-06-08 11:46:36 -0400

    add inpainting with python

--- inpainting/src/depthimage_inpainting.py
+++ inpainting/src/depthimage_inpainting.py
@@ -4,13 +4,14 @@ import cv2
 import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+import numpy as np
 class Inpainting():
     def __init__(self):
 
         # objects
         self.bridge = CvBridge()
-        sub = rospy.Subscriber("/camera/depth/image_raw", Image, self.depth_callback)
-        pub = rospy.Publisher("/camera/depth/inpainted", Image)
+        self.sub = rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.depth_callback)
+        self.pub = rospy.Publisher("/camera/depth/inpainted", Image, queue_size=1)
 
         # data
         self.depthimage = None
@@ -25,20 +26,18 @@ class Inpainting():
     def inpaint(self):
         # TODO: Aplicar canny para generar una mascara de los bordes, 8bit input image. Que tenemos nosotros?
         depth_image = self.depthimage
-        edges = cv2.Canny(depth_image, 100, 200)
-
-        # TODO: Con la mascara y la imagen, aplicar inpainting
+        ratio = np.amax(depth_image) / 256
+        depth_image = (depth_image/ratio).astype('uint8')
+        _, edges = cv2.threshold(depth_image, 0, 255, cv2.THRESH_BINARY_INV)
         dst = cv2.inpaint(depth_image, edges, 3, cv2.INPAINT_TELEA)
-
-        # TODO: Transformar la imagen inpainted a sensor_msgs.msg.Image
-
-        # TODO: Publicar la imagen correcta
-        pass
-
+        dst = (dst).astype('uint16')*ratio
+        msg = self.bridge.cv2_to_imgmsg(dst, encoding="passthrough")
+        self.pub.publish(msg)
 
 if __name__=="__main__":
     try:
         rospy.init_node('inpainting')
+        time.sleep(1)
         inpaint = Inpainting()
         time.sleep(1)
         while not rospy.is_shutdown():
